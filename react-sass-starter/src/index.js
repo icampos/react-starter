f81136c72113a708da2b6d7658d00be2eb76c9db
Isaac <isaac.camposcruz@gmail.com>
2018-06-05 18:54:55 -0600

    Initial Commit

---
+++ react-sass-starter/src/index.js
@@ -0,0 +1,8 @@
+import React, {Component} from 'react';
+import ReactDom from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+
+import App from './Components/App';
+
+ReactDom.render(<BrowserRouter><App/></BrowserRouter>, document.getElementById('root'));
+
